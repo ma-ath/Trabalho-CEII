f6f9d635517bb4cff73ed7b555bfd58803f9dd7b
Matheus Lima <mathlima@poli.ufrj.br>
2017-09-30 17:04:39 -0300

    main
    esses sao os arquivos main do trabalho, estao sendo reescritos em c++

---
+++ Nosso Programa/main.cpp
@@ -0,0 +1,14 @@
+/*
+ *  Arquivo main. Esse arquivo tem por objetivo re-implementar o programa mna1 so que de forma organizada
+ */
+
+#include <stdio.h>
+#include "main.hpp"
+
+int main()
+{
+
+
+
+ return 0;
+}
