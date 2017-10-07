a9a87a5a28450531a496f06679b91313eb691c7e
Matheus Lima <mathlima@poli.ufrj.br>
2017-10-07 17:47:36 -0300

    VERSAO 1.0
    VERSAO 1.0 CRIADA FUNCIONANDO SEM WARNINGS - NESSA VERSÃO NOS TEMOS O PROGRAMA DO MOREIRAO ORGANIZADO

---
+++ Nosso Programa/Separado em Arquivos/Versao1.0/constantes.h
@@ -0,0 +1,7 @@
+#define MAX_LINHA 80
+#define MAX_NOME 11
+#define MAX_ELEM 50
+#define MAX_NOS 50
+#define TOLG 1e-9
+#define DEBUG
+#define versao "1.0 07/10/2017"
--- Nosso Programa/Separado em Arquivos/Versao1.0/tipos.h
+++ Nosso Programa/Separado em Arquivos/Versao1.0/tipos.h
@@ -1,13 +1,20 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <conio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <math.h>
-*/
+
 #include "constantes.h"
-typedef struct elemento { /* Elemento do netlist */
+
+#ifndef _TIPOS_H_
+#define _TIPOS_H_
+
+typedef struct elemento /* Elemento do netlist */
+{
   char nome[MAX_NOME];
   double valor;
   int a,b,c,d,x,y;
 } elemento;
+
+#endif
