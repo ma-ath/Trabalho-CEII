1ac5d2febc7a0777e9e0461f2fef1cbcc7533575
Matheus Lima <mathlima@poli.ufrj.br>
2017-10-07 15:32:02 -0300

    estampas.h
    fiz esse arquivo com a classe estampas, com os metodos das montagens da estampas no programa (para despoluir o código)

---
+++ Nosso Programa/estampas.h
@@ -0,0 +1,102 @@
+/* Monta estampas */
+using namespace std;
+
+class estampas
+{
+public:
+    this* R(int i);
+    this* G(int i);
+    this* I(int i);
+    this* V(int i);
+    this* E(int i);
+    this* F(int i);
+    this* H(int i);
+    this* O(int i);
+
+    this* MostrarEstampas();
+}
+
+
+
+
+
+/*TIRADO DO MNA1*/
+/*
+for (i=1; i<=ne; i++)
+{
+  tipo=netlist[i].nome[0];
+  if (tipo=='R') {
+    g=1/netlist[i].valor;
+    Yn[netlist[i].a][netlist[i].a]+=g;
+    Yn[netlist[i].b][netlist[i].b]+=g;
+    Yn[netlist[i].a][netlist[i].b]-=g;
+    Yn[netlist[i].b][netlist[i].a]-=g;
+  }
+  else if (tipo=='G') {
+    g=netlist[i].valor;
+    Yn[netlist[i].a][netlist[i].c]+=g;
+    Yn[netlist[i].b][netlist[i].d]+=g;
+    Yn[netlist[i].a][netlist[i].d]-=g;
+    Yn[netlist[i].b][netlist[i].c]-=g;
+  }
+  else if (tipo=='I') {
+    g=netlist[i].valor;
+    Yn[netlist[i].a][nv+1]-=g;
+    Yn[netlist[i].b][nv+1]+=g;
+  }
+  else if (tipo=='V') {
+    Yn[netlist[i].a][netlist[i].x]+=1;
+    Yn[netlist[i].b][netlist[i].x]-=1;
+    Yn[netlist[i].x][netlist[i].a]-=1;
+    Yn[netlist[i].x][netlist[i].b]+=1;
+    Yn[netlist[i].x][nv+1]-=netlist[i].valor;
+  }
+  else if (tipo=='E') {
+    g=netlist[i].valor;
+    Yn[netlist[i].a][netlist[i].x]+=1;
+    Yn[netlist[i].b][netlist[i].x]-=1;
+    Yn[netlist[i].x][netlist[i].a]-=1;
+    Yn[netlist[i].x][netlist[i].b]+=1;
+    Yn[netlist[i].x][netlist[i].c]+=g;
+    Yn[netlist[i].x][netlist[i].d]-=g;
+  }
+  else if (tipo=='F') {
+    g=netlist[i].valor;
+    Yn[netlist[i].a][netlist[i].x]+=g;
+    Yn[netlist[i].b][netlist[i].x]-=g;
+    Yn[netlist[i].c][netlist[i].x]+=1;
+    Yn[netlist[i].d][netlist[i].x]-=1;
+    Yn[netlist[i].x][netlist[i].c]-=1;
+    Yn[netlist[i].x][netlist[i].d]+=1;
+  }
+  else if (tipo=='H') {
+    g=netlist[i].valor;
+    Yn[netlist[i].a][netlist[i].y]+=1;
+    Yn[netlist[i].b][netlist[i].y]-=1;
+    Yn[netlist[i].c][netlist[i].x]+=1;
+    Yn[netlist[i].d][netlist[i].x]-=1;
+    Yn[netlist[i].y][netlist[i].a]-=1;
+    Yn[netlist[i].y][netlist[i].b]+=1;
+    Yn[netlist[i].x][netlist[i].c]-=1;
+    Yn[netlist[i].x][netlist[i].d]+=1;
+    Yn[netlist[i].y][netlist[i].x]+=g;
+  }
+  else if (tipo=='O') {
+    Yn[netlist[i].a][netlist[i].x]+=1;
+    Yn[netlist[i].b][netlist[i].x]-=1;
+    Yn[netlist[i].x][netlist[i].c]+=1;
+    Yn[netlist[i].x][netlist[i].d]-=1;
+  }
+#ifdef DEBUG
+  / Opcional: Mostra o sistema apos a montagem da estampa /
+  printf("Sistema apos a estampa de %s\n",netlist[i].nome);
+  for (k=1; k<=nv; k++) {
+    for (j=1; j<=nv+1; j++)
+      if (Yn[k][j]!=0) printf("%+3.1f ",Yn[k][j]);
+      else printf(" ... ");
+    printf("\n");
+  }
+  getch();
+#endif
+}
+*/
