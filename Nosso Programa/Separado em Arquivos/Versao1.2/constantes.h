0c3253b96c734dcd7be45ad8f8584181a153161d
Matheus Lima <mathlima@poli.ufrj.br>
2017-10-09 19:47:33 -0300

    Versao Final do programa analisando no tempo, circuito resistivo !

---
+++ Nosso Programa/Separado em Arquivos/Versao1.2/constantes.h
@@ -0,0 +1,9 @@
+#define MAX_LINHA       80
+#define MAX_NOME        11
+#define MAX_ELEM        50
+#define MAX_NOS         50
+#define MAX_NOME_FONTE  10
+#define PI              (3.141592653589793)
+#define TOLG 1e-9
+//#define DEBUG
+#define versao "1.0 07/10/2017"
---
+++ Nosso Programa/Separado em Arquivos/Versao1.2/tipos.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+#include "constantes.h"
+
+#ifndef _TIPOS_H_
+#define _TIPOS_H_
+
+typedef struct elemento /* Elemento do netlist */
+{
+  char nome[MAX_NOME];
+  double valor; /*usado tbm como DC "valor", SIN "nivel continuo", PULSE "amplitude1"*/
+  int a,b,c,d,x,y;
+  /*a partir daqui so vai usar se for uma fonte que n eh dc: I ou V que tem <parametro>*/
+  char fonte[MAX_NOME_FONTE]; //se eh DC, SIN, ou PULSE
+  double amplitude /*anplitude2 para pulse*/, freq, atraso, amortecimento,
+          defasagem, ciclo, tempoSubida, tempoDescida, tempoLigada, periodo;
+} elemento;
+
+#endif
