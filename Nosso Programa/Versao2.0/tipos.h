5da3780b8716bbaa00ac2ff1e7458cbb30d73e4b
Matheus Lima <mathlima@poli.ufrj.br>
2017-10-22 00:04:07 -0200

    Versão 2.0 is out now!
    Agora finalmente simulando capacitores e indutores!
    
    **Whats New**
       Full simulação com capacitores e indutores com método de trapézio funcionando!
       Calculo de ponto de operação funcionando para análise de circuitos indutivos/capacitivos
        Nova função de barrinha de progresso que indica quantos porcento da simulação já rodou!
        Novos esquemáticos pra gente testar, agora comentados e desenhadinhos!
        Agora opção DEBUG tbm esconde várias coisas desnecessárias do moreirão!
        Mais constantes, variaveis globais e diversão pra você!

---
+++ Nosso Programa/Versao2.0/tipos.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+#include "constantes.h"
+
+#ifndef _TIPOS_H_
+#define _TIPOS_H_
+
+typedef struct elemento /* Elemento do netlist */
+{
+  char nome[MAX_NOME];
+  double valor; /*usado tbm como DC "valor", SIN "nivel continuo", PULSE "amplitude1"*/
+  int a,b,c,d,x,y;
+  /*a partir daqui so vai usar se for uma fonte que n eh dc: I ou V que tem <parametro>*/
+  char fonte[MAX_NOME_FONTE]; //se eh DC, SIN, ou PULSE
+  double amplitude /*anplitude2 para pulse*/, freq, atraso, amortecimento,
+          defasagem, ciclo, tempoSubida, tempoDescida, tempoLigada, periodo;
+
+  /*Aqui sao os parametros de memoria para o capacitor e indutor no tempo*/
+  double vt0; /*Tensao anterior sobre o capacitor/indutor*/
+  double jt0; /*Corrente anterior sobre o capacitor/indutor*/
+} elemento;
+
+#endif
